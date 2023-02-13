ad6f946bddc69c292a79519347de8bf4626bb53e
Antek93 <antonio.savino1993@gmail.com>
2023-02-13 17:09:50 +0100

    aggiunto primo elemento del main

--- src/components/Footer/AppFooter.vue
+++ src/components/Footer/AppFooter.vue
@@ -20,9 +20,6 @@ export default {
 </script>
 
 <template>
-<div>
-    ciao
-</div>
 </template>
 
 <style lang="scss">
--- src/components/Main/AppMain.vue
+++ src/components/Main/AppMain.vue
@@ -4,27 +4,59 @@ import { store } from "../../store"
 
 export default {
     name: 'AppMain',
-    data () {
+    data() {
         return {
             msg: 'Ciao',
             store
         }
     },
-    created () {
-        
+    created() {
+
     },
     methods: {
     },
-    
+
 };
 </script>
 
 <template>
+    <!-- CONTAINER PRINCIPALE CON GIOCO IN USCITA -->
+    <div class="mainContainer container-fluid text-light">
 
-<div>
-    ciao
-</div>
+        <!-- CONTAINER CON "DISCLAIMER" + "TITOLO GIOCO IN USCITA" + "TASTO SHOPNOW" -->
+        <div class="container">
+
+            <!-- DISCLAIMER -->
+            <div>
+                Upcoming Stock
+            </div>
+            <!-- TITOLO GIOCO IN USCITA -->
+            <div>
+                <h3>
+                    Resident Evil 3
+                </h3>
+                <h3>
+                    Remake
+                </h3>
+            </div>
+            <!-- TASTO SHOP NOW -->
+            <div>
+                <a href="#">Shop Now</a>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.mainContainer {
+    background-image: url("../../assets/bph/mainwallpaper.jpg");
+    background-size: cover;
+    height: 100vh;
+    .container {
+        width: 70%;
+        margin: 0 auto;
+        height: 100px;
+    }
+
+}
 </style>

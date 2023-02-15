6a488f98710f624be7a625204cbe19110ef44104
Antek93 <antonio.savino1993@gmail.com>
2023-02-15 17:25:59 +0100

    aggiunto hover al footer

--- src/components/Footer/AppFooter.vue
+++ src/components/Footer/AppFooter.vue
@@ -36,7 +36,7 @@ export default {
             <div class="footContainer d-flex justify-content-between">
                 <div class="address">
                     <div>Address Info</div>
-                    <ul v-for="element in addressInfo">
+                    <ul class="ulStyle" v-for="element in addressInfo">
                         <li>
                             <a href="#">{{ element }}</a>
                         </li>
@@ -44,7 +44,7 @@ export default {
                 </div>
                 <div class="info">
                     <div>Information</div>
-                    <ul v-for="element in info">
+                    <ul class="ulStyle" v-for="element in info">
                         <li>
                             <a href="#">{{ element }}</a>
                         </li>
@@ -52,7 +52,7 @@ export default {
                 </div>
                 <div class="extras d-flex flex-column">
                     <div>Extras</div>
-                    <ul v-for="element in extras">
+                    <ul class="ulStyle" v-for="element in extras">
                         <li>
                             <a href="#">{{ element }}</a>
                         </li>
@@ -82,10 +82,10 @@ export default {
                 </div>
             </div>
         </div>
-        <hr>
+        <hr id="hrStyle">
         <div class="downFooter d-flex justify-content-between">
             <div class="copyright">
-                <a href="#">Copyright</a>
+                <a href="#">Copyright @ 2023 Codezeel</a>
             </div>
             <div class="creditCards d-flex justify-content-evenly">
                 <div class="payBox">
@@ -94,15 +94,15 @@ export default {
                 <div class="payBox">
                     <img src="../../assets/bph/discover.png" alt="">
                 </div>
-              
+
                 <div class="payBox">
                     <img src="../../assets/bph/mastercard.png" alt="">
                 </div>
-              
+
                 <div class="payBox">
                     <img src="../../assets/bph/paypal.png" alt="">
                 </div>
-              
+
                 <div class="payBox">
                     <img src="../../assets/bph/visa.png" alt="">
                 </div>
@@ -113,7 +113,7 @@ export default {
 
 <style lang="scss" scoped>
 .container-fluid {
-    height: 45vh;
+    height: 50vh;
     background-image: url('../../assets/bph/footer-bg.png');
     background-size: cover;
     --bs-gutter-x: 0rem;
@@ -132,7 +132,11 @@ export default {
                 a {
                     text-decoration: none;
                     color: white;
-                    font-size: 15px;
+                    font-size: 17px;
+                }
+
+                a:hover {
+                    color: rgb(249, 207, 0);
                 }
             }
         }
@@ -143,39 +147,58 @@ export default {
     .extras div:first-child,
     .info div:first-child {
         padding: 20px 0px;
-        font-size: 20px;
+        font-size: 25px;
     }
 
     hr {
         width: 100%;
         margin: 0 auto;
         border: 0.5px solid white;
+        margin-top: 101px;
     }
 
     .downFooter {
         width: 80%;
         margin: 0 auto;
         color: white;
+
         div {
-            a{
+            a {
                 text-decoration: none;
                 color: white;
             }
+
         }
     }
+
     .payBox {
-        width: calc((100% / 5) - 20px) ;
+        width: calc((100% / 5) - 20px);
         height: 80%;
+
         img {
             width: 100%;
             height: 100%;
         }
     }
+
     .copyright {
         width: 75%;
     }
+
     .creditCards {
         width: 25%;
     }
 }
+
+.ulStyle {
+    height: 17px;
+}
+
+p {
+    font-size: 17px;
+}
+
+// #hrStyle {
+//     margin-top: 101px;
+// }
 </style>

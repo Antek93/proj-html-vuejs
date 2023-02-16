fe3892c7ae56b4fa3d0bfd751a619965877a7467
Antek93 <antonio.savino1993@gmail.com>
2023-02-16 13:44:14 +0100

    inserita nuova mappa concettuale

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,69 @@ export default {
     data() {
         return {
             phoneNumber: "24/7 . 800-8754-987",
+            slides: [
+                {
+                    image: '../../../src/assets/bph/04-768x768.jpg',
+                    rating: '★★★☆☆',
+                    title: 'Shopping Mahjong conncet',
+                    price: '100.00 - £1̶̶8̶̶0̶̶ ',
+                }, {
+                    image: '../../../src/assets/bph/01-768x768.jpg',
+                    rating: '★★★★★',
+                    title: 'BangBangShooting',
+                    price: '83.00 - £90.00',
+                }, {
+                    image: '../../../src/assets/bph/03-768x768.jpg',
+                    rating: '★★★☆☆',
+                    title: 'Buddy and Lucky Solitaire',
+                    price: "74.00",
+                }, {
+                    image: '../../../src/assets/bph/11-768x768.jpg',
+                    rating: '★★★★☆',
+                    title: 'Taishou x Alice Epilogue',
+                    price: '160.00',
+                },
+                {
+                    image: '../../../src/assets/bph/01-768x768.jpg',
+                    rating: '★★★★★',
+                    title: 'BangBangShooting',
+                    price: '33.00',
+                }, {
+                    image: '../../../src/assets/bph/05-768x768.jpg',
+                    rating: '★★☆☆☆',
+                    title: 'Shopping Mahjong conncet',
+                    price: '19.00',
+                }, {
+                    image: '../../../src/assets/bph/08-768x768.jpg',
+                    rating: '★★★☆☆',
+                    title: 'Taishou x Alice Epilogue',
+                    price: "28.00",
+                }, {
+                    image: '../../../src/assets/bph/13-768x768.jpg',
+                    rating: '★★★★☆',
+                    title: 'Buddy and Lucky Solitaire',
+                    price: '74.00',
+                }
+            ],
+            photos: [
+                {
+                    image: '../../../src/assets/bph/18-768x768.jpg',
+                }, {
+                    image: '../../../src/assets/bph/16-768x768.jpg',
+                }, {
+                    image: '../../../src/assets/bph/17-768x768.jpg',
+                }, {
+                    image: '../../../src/assets/bph/14-768x768.jpg',
+                }, {
+                    image: '../../../src/assets/bph/10-768x768.jpg',
+                }, {
+                    image: '../../../src/assets/bph/07-768x768.jpg',
+                }, {
+                    image: '../../../src/assets/bph/08-768x768.jpg',
+                }, {
+                    image: '../../../src/assets/bph/13-768x768.jpg',
+                }
+            ],
             navigator: [
                 'Home',
                 'Shop',
@@ -37,6 +100,47 @@ export default {
                 'My account',
                 'About us'
             ],
+            benefits: [
+                {
+                    title: "Free Delivery",
+                    subtitle: "Free Shipping On All Order",
+                    icon: "fa-solid fa-truck-fast"
+                },
+                {
+                    title: "Money Return",
+                    subtitle: "Back Guarantee in 7 days",
+                    icon: "fa-solid fa-sack-dollar",
+                },
+                {
+                    title: "Member Discount",
+                    subtitle: "On every order over $130.00",
+                    icon: 'fa-solid fa-percent',
+                },
+                {
+                    title: "Return Policy",
+                    subtitle: "Support 24 hours a day",
+                    icon: 'fa-solid fa-hand-holding-dollar',
+                }
+            ],
+            team: [
+                {
+                    image: '../../../src/assets/bph/t2-1-100x100_left.jpg',
+                    name: 'Luies Charls',
+                    job: '   (Web Designer)'
+                }, {
+                    image: '../../../src/assets/bph/t4-1-100x100_left.jpg',
+                    name: 'Mark Harry',
+                    job: '   (Front-end Developer)',
+                }, {
+                    image: '../../../src/assets/bph/t5-1-100x100_left.jpg',
+                    name: 'Harry Mark',
+                    job: '   (Project Manager)',
+                }, {
+                    image: '../../../src/assets/bph/t6-1-100x100_left.jpg',
+                    name: '',
+                    job: '',
+                }
+            ],
         }
     },
     created() {
@@ -62,13 +166,21 @@ export default {
         </div>
     </div>
     <div>
-        <AppHeader :navigator="navigator" :phoneNumber="phoneNumber" />
+        <AppHeader
+        :navigator="navigator"
+        :phoneNumber="phoneNumber" />
     </div>
     <div>
-        <AppMain />
+        <AppMain
+        :benefits="benefits"
+        :slides="slides"
+        :photos="photos"
+        :team="team" />
     </div>
     <div>
-        <AppFooter :extras="extras" :addressInfo="addressInfo" :info="info" />
+        <AppFooter :extras="extras"
+        :addressInfo="addressInfo"
+        :info="info" />
     </div>
 </template>
 
--- src/components/Main/AppMain.vue
+++ src/components/Main/AppMain.vue
@@ -14,111 +14,6 @@ export default {
     name: 'AppMain',
     data() {
         return {
-            benefits: [
-                {
-                    title: "Free Delivery",
-                    subtitle: "Free Shipping On All Order",
-                    icon: "fa-solid fa-truck-fast"
-                },
-                {
-                    title: "Money Return",
-                    subtitle: "Back Guarantee in 7 days",
-                    icon: "fa-solid fa-sack-dollar",
-                },
-                {
-                    title: "Member Discount",
-                    subtitle: "On every order over $130.00",
-                    icon: 'fa-solid fa-percent',
-                },
-                {
-                    title: "Return Policy",
-                    subtitle: "Support 24 hours a day",
-                    icon: 'fa-solid fa-hand-holding-dollar',
-                }
-            ],
-            slides: [
-                {
-                    image: '../../../src/assets/bph/04-768x768.jpg',
-                    rating: '★★★☆☆',
-                    title: 'Shopping Mahjong conncet',
-                    price: '100.00 - £1̶̶8̶̶0̶̶ ',
-                }, {
-                    image: '../../../src/assets/bph/01-768x768.jpg',
-                    rating: '★★★★★',
-                    title: 'BangBangShooting',
-                    price: '83.00 - £90.00',
-                }, {
-                    image: '../../../src/assets/bph/03-768x768.jpg',
-                    rating: '★★★☆☆',
-                    title: 'Buddy and Lucky Solitaire',
-                    price: "74.00",
-                }, {
-                    image: '../../../src/assets/bph/11-768x768.jpg',
-                    rating: '★★★★☆',
-                    title: 'Taishou x Alice Epilogue',
-                    price: '160.00',
-                },
-                {
-                    image: '../../../src/assets/bph/01-768x768.jpg',
-                    rating: '★★★★★',
-                    title: 'BangBangShooting',
-                    price: '33.00',
-                }, {
-                    image: '../../../src/assets/bph/05-768x768.jpg',
-                    rating: '★★☆☆☆',
-                    title: 'Shopping Mahjong conncet',
-                    price: '19.00',
-                }, {
-                    image: '../../../src/assets/bph/08-768x768.jpg',
-                    rating: '★★★☆☆',
-                    title: 'Taishou x Alice Epilogue',
-                    price: "28.00",
-                }, {
-                    image: '../../../src/assets/bph/13-768x768.jpg',
-                    rating: '★★★★☆',
-                    title: 'Buddy and Lucky Solitaire',
-                    price: '74.00',
-                }
-            ],
-            photos: [
-                {
-                    image: '../../../src/assets/bph/18-768x768.jpg',
-                }, {
-                    image: '../../../src/assets/bph/16-768x768.jpg',
-                }, {
-                    image: '../../../src/assets/bph/17-768x768.jpg',
-                }, {
-                    image: '../../../src/assets/bph/14-768x768.jpg',
-                }, {
-                    image: '../../../src/assets/bph/10-768x768.jpg',
-                }, {
-                    image: '../../../src/assets/bph/07-768x768.jpg',
-                }, {
-                    image: '../../../src/assets/bph/08-768x768.jpg',
-                }, {
-                    image: '../../../src/assets/bph/13-768x768.jpg',
-                }
-            ],
-            team: [
-                {
-                    image: '../../../src/assets/bph/t2-1-100x100_left.jpg',
-                    name: 'Luies Charls',
-                    job: '   (Web Designer)'
-                }, {
-                    image: '../../../src/assets/bph/t4-1-100x100_left.jpg',
-                    name: 'Mark Harry',
-                    job: '   (Front-end Developer)',
-                }, {
-                    image: '../../../src/assets/bph/t5-1-100x100_left.jpg',
-                    name: 'Harry Mark',
-                    job: '   (Project Manager)',
-                }, {
-                    image: '../../../src/assets/bph/t6-1-100x100_left.jpg',
-                    name: '',
-                    job: '',
-                }
-            ],
-
         }
 
     },
@@ -139,7 +34,23 @@ export default {
         teamInfo,
         newGameBlog,
         sponsorIcon
-    }
+    },
+    props: {
+        benefits: {
+            type: Array,
+        },
+        slides: {
+            type: Array,
+        },
+        photos: {
+            type: Array,
+        },
+        team: {
+            type: Array,
+        }
+    },
+    
+
 
 };
 </script>
